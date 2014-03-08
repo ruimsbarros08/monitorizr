2659451ff5c345b07eb87850e2167e99841230d2
Rui Barros <ruimsbarros08@gmail.com>
2014-03-08 21:39:57 +0000

    time updated

--- js/app/services/mainSrvc.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.factory('mainSrvc', function ($q, $http) {
-	var deferred = $q.defer();
-    var url = "http://localhost/api/sections/commercial/allprofiles";
-    return $http.get(url).then(function(data){
-    	deferred.resolve(data);
-    });
-});
