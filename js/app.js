43ac4f42894a45b19dd6317ea7ed144bb434271b
Rui Barros <ruimsbarros08@gmail.com>
2014-03-11 00:45:33 +0000

    win

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var end = (new Date()).getTime();
 
 var app = angular.module('parfois_monitor', ['ui.router', 'chieffancypants.loadingBar', "highcharts-ng"]);
 
+/*
 app.factory("instant_cons", function($q, $http) {
     var deferred = $q.defer();
     var url = BASE_URL + "/emoncms/feed/data.json?&id=3&start="+old_t+"&end="+new_t+"&dp=500&apikey=dc1c0ce300c4859934ec0f868671b2e5";
@@ -18,13 +19,14 @@ app.factory("instant_cons", function($q, $http) {
 app.controller("main_info", function ($scope, $timeout, instant_cons) {
 
       $scope.cons = 0;
-      var old_t = end;
+      var old_t = (new Date()).getTime();
   
       (function update() {
         $timeout(update, 5000);
         var new_t = (new Date()).getTime();
         $scope.cons = instant_cons;
-        var old_t = (new Date()).getTime();
+        var old_t = new_t;
       }());
 
 });
+*/
