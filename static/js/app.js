66fa43c4e12b7bf52444065e0984a883c9746f8a
Rui Barros <ruibarros@SELEQ.fe.up.pt>
2014-05-21 17:20:30 +0100

    Carousel updated

--- static/js/app.js
+++ static/js/app.js
@@ -3,5 +3,5 @@
 var start = 1388534400000;
 var end = (new Date()).getTime();
 
-var app = angular.module('monitor', ['ui.router', 'chieffancypants.loadingBar', "highcharts-ng", "pageslide-directive", 'ui.bootstrap']);
+var app = angular.module('monitor', ['ui.router', 'chieffancypants.loadingBar', "highcharts-ng", "pageslide-directive", 'ui.bootstrap', "ngSanitize"]);
 
--- static/js/homeCtrl.js
+++ static/js/homeCtrl.js
@@ -5,9 +5,9 @@ app.controller("homeCtrl", function ($scope) {
 	$scope.myInterval = 3000;
   	var slides = $scope.slides = [];
 
-  	$scope.slides.push({image: 'img/carousel/energy.jpg'});
-  	$scope.slides.push({image: 'img/carousel/temp.jpg'});
-  	$scope.slides.push({image: 'img/carousel/humidity.jpg'});
+  	$scope.slides.push({text: "<a href='#/instant' class='btn btn-success btn-sm'>Energia</a>", image: 'img/carousel/energy.jpg'});
+  	$scope.slides.push({text: "<a href='#/temp' class='btn btn-success btn-sm'>Temperatura</a>", image: 'img/carousel/temp.jpg'});
+  	$scope.slides.push({text: "<a href='#/hum' class='btn btn-success btn-sm'>Humidade</a>", image: 'img/carousel/humidity.jpg'});
 
   	$scope.setActive = function(idx) {
     $scope.slides[idx].active=true;
