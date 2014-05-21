985315bee2a0075470dd692962de2db1693566ec
Rui Barros <ruibarros@SELEQ.fe.up.pt>
2014-05-21 15:41:36 +0100

    deleted redundant files

--- js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var start = 1388534400000;
-var end = (new Date()).getTime();
-
-var app = angular.module('monitor', ['ui.router', 'chieffancypants.loadingBar', "highcharts-ng"]);
-
--- js/variables.js
+++
@@ -1,6 +0,0 @@
-var BASE_URL = "http://openg.fe.up.pt";
-var apiKey = "628f6465a180a62e1173e530ca19ba9e";
-var feedId = 4;
-
-
-
